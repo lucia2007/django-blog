61f7ad694ccff0db1a1aa6bb3c57c3de4da7e3f9
Lucia Ferencik <lferencikova@gmail.com>
2023-04-13 16:20:10 +0000

    Bug-fix: missing class from which to inherit

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.views import generic
 from .models import Post
 
 
-class PostList():
+class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
