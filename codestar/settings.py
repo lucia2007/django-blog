eca325cfd99d8385a9de72bc5da13cc2f24cdc6f
Lucia Ferencik <lferencikova@gmail.com>
2023-04-17 11:51:32 +0000

    Set dynamic debug settings (change env.py!)

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ 
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
